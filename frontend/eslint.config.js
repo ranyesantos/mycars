a43fe9472ed9e9d633c91c8415fd73eb4885aa4a
ranyesantos <lucasgts17@gmail.com>
2026-05-15 20:33:36 -0300

    chore: add ESLint and Prettier configs for backend and frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,23 @@
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaVersion: 2022, sourceType: 'module', ecmaFeatures: { jsx: true } },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'off',
+    },
+  },
+  prettier,
+]
