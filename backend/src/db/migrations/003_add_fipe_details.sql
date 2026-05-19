de6b5c0d770ab16d0c4a55954a8a5072b4a0a934
ranyesantos <lucasgts17@gmail.com>
2026-05-19 19:57:59 -0300

    feat: add FIPE detail columns to vehicle_years table

---
+++ backend/src/db/migrations/003_add_fipe_details.sql
@@ -0,0 +1,4 @@
+ALTER TABLE vehicle_years ADD COLUMN fuel TEXT;
+ALTER TABLE vehicle_years ADD COLUMN reference_month TEXT;
+ALTER TABLE vehicle_years ADD COLUMN fuel_acronym TEXT;
+ALTER TABLE vehicle_years ADD COLUMN fetched_at DATETIME;
